1cc5fe8c2591a0aeb847c68da5ff630a1792f9cd
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-07-12 16:38:33 +0700

    fix(get clang): Wrong variable

--- get_clang.sh
+++ get_clang.sh
@@ -5,14 +5,14 @@ CLANG_NAME=$(grep -oE 'clang-r[0-9]+[a-z]*' "$KERNEL_DIR/build.config.common" | 
 if [ -z "$CLANG_NAME" ]; then
  echo "Error: Invaid Clang '$CLANG_NAME'."
  exit 1
-elif ! git clone --filter=blob:none --no-checkout https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 aosp_clang_meta; then
+elif ! git clone --filter=blob:none --no-checkout https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 aosp_clang; then
  echo "Error: Can not fetch AOSP history from Google."
  exit 1
 fi
-cd aosp_clang
 
 CLANG_TARGET=$(git log -n 1 --all --diff-filter=AM --format="%H" -- "$CLANG_NAME")
 
+cd aosp_clang
 if [ -z "$CLANG_TARGET" ]; then
     echo "Error: '$CLANG_NAME' not found in AOSP history."
     cd .. && rm -rf aosp_clang
@@ -24,4 +24,4 @@ git sparse-checkout set "$CLANG_NAME"
 git checkout "$CLANG_TARGET"
 
 mv "$CLANG_NAME" "$CLANG_DIR"
-cd .. && rm -rf aosp_clang_meta
+cd .. && rm -rf aosp_clang
