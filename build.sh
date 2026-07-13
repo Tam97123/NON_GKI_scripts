ea39159069c6e915d15d4acc6ff55d9173686768
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-07-13 14:33:08 +0700

    perf(build): Fix wrong directory

--- build.sh
+++ build.sh
@@ -75,7 +75,7 @@ build_without_gcc () {
 get_gcc() {
     echo "Downloading scripts..."
     if [ ! -f "get_gcc.sh" ]; then 
-     if ! curl -LO https://raw.githubusercontent.com/Tam97123/NON_GKI_scripts/refs/scripts/main/get_gcc.sh; then
+     if ! curl -LO https://raw.githubusercontent.com/Tam97123/NON_GKI_scripts/refs/heads/main/scripts/get_gcc.sh; then
       echo "Error: Can not download the file! Exiting..."
       exit 1
      fi
@@ -87,7 +87,7 @@ get_gcc() {
 get_clang () {
     echo "Downloading scripts..."
     if [ ! -f "get_clang.sh" ]; then 
-     if ! curl -LO https://raw.githubusercontent.com/Tam97123/NON_GKI_scripts/refs/scripts/main/get_clang.sh; then
+     if ! curl -LO https://raw.githubusercontent.com/Tam97123/NON_GKI_scripts/refs/heads/main/scripts/get_clang.sh; then
       echo "Error: Can not download the file! Exiting..."
       exit 1
      fi
