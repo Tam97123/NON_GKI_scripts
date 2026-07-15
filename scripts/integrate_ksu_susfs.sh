dcac4cf24cc7b931b5e06018335f564fca2814a1
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-07-15 16:26:25 +0700

    refactor(integrate ksu susfs): Remove check

--- scripts/integrate_ksu_susfs.sh
+++ scripts/integrate_ksu_susfs.sh
@@ -10,44 +10,38 @@ if [ ! -d "$KERNEL_DIR/KernelSU" ]; then
  fi
 fi
 
-# For UI, you can remove these or comment out
+# For UI only
 sed -i 's|$(subst ",,$(CONFIG_KSU_FULL_NAME_FORMAT))|%TAG_NAME%-%COMMIT_SHA%-t.me/noforce2pay/|' "$KERNEL_DIR/KernelSU/kernel/Kbuild"
 sed -i '/-dirty/d' "$KERNEL_DIR/KernelSU/kernel/Kbuild"
 
-# Patch SUSFS (temporary for non gki)
-if [[ "$VERSION" -lt "5" || ( "$VERSION" -eq "5" && "$PATCH_LEVEL" -eq "4" ) ]]; then
- if [ ! -f "$KERNEL_DIR/susfs_inline_hook_patches.sh" ]; then
-  echo "Downloading script..."
-  if ! curl -LO https://raw.githubusercontent.com/JackA1ltman/NonGKI_Kernel_Build_2nd/refs/heads/mainline/Patches/susfs_inline_hook_patches.sh; then
-   echo "Error: Can not download script."
-   exit 1
-  fi
-  chmod +x susfs_inline_hook_patches.sh && ./susfs_inline_hook_patches.sh
+# Patch SUSFS
+if [ ! -f "$KERNEL_DIR/susfs_inline_hook_patches.sh" ]; then
+ echo "Downloading script..."
+ if ! curl -LO https://raw.githubusercontent.com/JackA1ltman/NonGKI_Kernel_Build_2nd/refs/heads/mainline/Patches/susfs_inline_hook_patches.sh; then
+  echo "Error: Can not download script."
+  exit 1
  fi
-
+ chmod +x susfs_inline_hook_patches.sh && ./susfs_inline_hook_patches.sh
+fi
  if [ ! -f "$KERNEL_DIR/susfs_patch_to_$VERSION.$PATCH_LEVEL.patch" ]; then
-  echo "Downloading patch..."
-  if ! curl -LO https://raw.githubusercontent.com/JackA1ltman/NonGKI_Kernel_Build_2nd/refs/heads/mainline/Patches/Patch/susfs_patch_to_$VERSION.$PATCH_LEVEL.patch; then
-   echo "Error: Can not download patch"
-   exit 1
-  fi
-  patch -p1 < "susfs_patch_to_$VERSION.$PATCH_LEVEL.patch" || true
+ echo "Downloading patch..."
+ if ! curl -LO https://raw.githubusercontent.com/JackA1ltman/NonGKI_Kernel_Build_2nd/refs/heads/mainline/Patches/Patch/susfs_patch_to_$VERSION.$PATCH_LEVEL.patch; then
+  echo "Error: Can not download patch"
+  exit 1
  fi
-else
- echo "Temporary not support GKI kernel. Aborting..."
- exit 1
+ patch -p1 < "susfs_patch_to_$VERSION.$PATCH_LEVEL.patch" || true
 fi
 
 move_rejects() {
+ mkdir -p "$REJECT_DIR"
  find . -type f -name "*.rej" -print0 | while IFS= read -r -d $'\0' rej_file; do
-  mkdir -p "$REJECT_DIR"
   local rej_dir=$(dirname "$rej_file")
   mkdir -p "$REJECT_DIR/$rej_dir" 
   mv "$rej_file" "$REJECT_DIR/$rej_dir/"
  
   local orig_file="${rej_file%.rej}.orig"
   if [ -f "$orig_file" ]; then
-    mv "$orig_file" "$REJECT_DIR/$rej_dir/"
+    cp "$orig_file" "$REJECT_DIR/$rej_dir/"
   fi
  done
 }
